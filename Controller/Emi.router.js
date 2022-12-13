59a682d5c46a2607b7c684f36d9cfd289a3b4a76
Durgashankar Pal <durgashankarpal14285@gmail.com>
2022-12-13 14:12:13 +0530

    backend done

--- Controller/Emi.router.js
+++ Controller/Emi.router.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const EmiRouter = express.Router()
-EmiRouter.post("/",async(req,res)=>{
-   const {loan,anualInterest,totlamonth} = req.body
-   let x = Number(anualInterest)/12/100
-   let emi = (Number(loan)*x*((1+x)/Number(totlamonth)))/(((1+x)/Number(totlamonth)-1))
-   res.status(200).send(emi.toString())
+EmiRouter.post("/", async (req, res) => {
+    const { loan, anualInterest, totlamonth } = req.body
+    if (anualInterest == Number(anualInterest)) {
+        let r = anualInterest / 12 / 100;
+        let emi = Number((loan * r * ((1 + r) ** totlamonth) / ((1 + r) ** totlamonth - 1)).toFixed(2));
+        let payableInterest = Number(((emi * totlamonth) - loan).toFixed());
+        let total = Number((emi * totlamonth).toFixed(2));
+        return res.status(200).send({ emi: emi, interest: payableInterest, total: total });
+    }
+    return res.status(401).send("Please provide valid Input")
 })
-module.exports =EmiRouter
+module.exports = EmiRouter
 
 // EMI:E = P x r x ( 1 + r )n / ( ( 1 + r )n - 1 ) 
