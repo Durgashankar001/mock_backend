319d2c18ff07fa21650d7f5214c92a7ae72f0aca
Durgashankar Pal <durgashankarpal14285@gmail.com>
2022-12-15 13:37:26 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const User = require("../Models/user.model")
+const User = require("../Models/product.model")
 const cors = require("cors")
 const connect = require("../Connect/Connect")
 const req = require("express/lib/request")
-const userRouter = require("../Controller/user.router")
-const EmiRouter = require("../Controller/Emi.router")
+const productRouter = require("../Controller/product.router")
+const bookMarkRouter = require("../Controller/bookmark.router")
 const PORT = process.env.PORT || 8000
 mongoose.set('strictQuery', true)
 const server = express()
 server.use(express.json())
 server.use(cors())
 
-server.get("/",async(req,res)=>{
+server.get("/", async (req, res) => {
     res.status(200).send("Hello Welcome to my server")
 })
-server.use("/user",userRouter)
-server.use("/emi",EmiRouter)
-server.post("/",async(req,res)=>{
+server.use("/product", productRouter)
+server.use("/bookmark", bookMarkRouter)
+server.post("/", async (req, res) => {
     const user = req.body
     console.log(user)
     const data = await User.create(user)
-    res.status(200).send({message:"data added successfully",data:data})
+    res.status(200).send({ message: "data added successfully", data: data })
 })
 server.listen(PORT, async () => {
     await connect()
