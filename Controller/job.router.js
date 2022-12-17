dcbe8206960379d8e4a6e9c6e45c69719ce62b53
Durgashankar Pal <durgashankarpal14285@gmail.com>
2022-12-17 13:48:42 +0530

    done

--- Controller/job.router.js
+++ Controller/job.router.js
@@ -11,19 +11,20 @@ jobRouter.post("/", async (req, res) => {
 })
 
 jobRouter.get("/", async (req, res) => {
-    const { sort, search, filter } = req.query
+    const { sort, search, filter, page = 1, limit = 10 } = req.query
     try {
+        let x = await Job.find()
         let newJob
         if (sort == "asc") {
-            newJob = await Job.find().sort({ 'createdAt': 1 })
+            newJob = await Job.find().sort({ 'createdAt': 1 }).skip(10 * (page - 1)).limit(limit)
         } else if (sort == "desc") {
-            newJob = await Job.find().sort({ 'createdAt': -1 })
+            newJob = await Job.find().sort({ 'createdAt': -1 }).skip(10 * (page - 1)).limit(limit)
         } else if (filter) {
-            newJob = await Job.find({ role: filter })
+            newJob = await Job.find({ role: filter }).skip(10 * (page - 1)).limit(limit)
         } else {
-            newJob = await Job.find()
+            newJob = await Job.find().skip(10 * (page - 1)).limit(limit)
         }
-        return res.status(200).send({ message: "Product get successfully", data: newJob })
+        return res.status(200).send({ message: "Product get successfully", data: newJob, totalPages: Math.ceil(x.length / limit) })
 
     } catch (e) {
         return res.status(500).send("Internal server error")
